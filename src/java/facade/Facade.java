fd678c3a7dcf423c7d9a427648278f51c3809039
Athinodoros <athinodoros.sgouromallis@hotmail.com>
2015-10-06 10:45:46 +0200

    added the second persistense unit and the class that takes care of it

--- src/java/facade/Facade.java
+++ src/java/facade/Facade.java
@@ -5,10 +5,26 @@
  */
 package facade;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 /**
  *
  * @author Athinodoros
  */
 public class Facade {
+    
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory(deploy.DeploymentConfiguration.PU_NAME);
+    
+    public String addPerson(String person) {
+        EntityManager em = emf.createEntityManager();
+        
+        em.getTransaction().begin();
+        
+        em.getTransaction().commit();
+        return "";
+    };
+   
     
 }

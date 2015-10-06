1398bbb0c016dbd0d80a38554fa278c1ea55c68a
fandul <rihards.pacevics@gmail.com>
2015-10-06 10:48:36 +0200

    interface
    created interface with basic CRUD methods

---
+++ src/java/facade/JpaInterface.java
@@ -0,0 +1,31 @@
+
+package facade;
+
+/**
+ *
+ * @author Rihards
+ */
+public interface JpaInterface {
+    
+    public String createPerson(Person pers);
+   
+    public String deletePerson(int id);
+    
+    public String updatePerson(int id);
+    
+    public Person getPerson (int id);
+   
+    public List<Person> getPersons ();
+    
+    public String createCompany(Company comp);
+   
+    public String deleteCompany(int cvr);
+    
+    public String updateCompany(int cvr);
+    
+    public Company getCompany (int cvr);
+   
+    public List<Company> getCompanies();
+  
+    
+}

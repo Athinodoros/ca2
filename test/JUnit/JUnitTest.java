044bc2b142697ab8bfd2456f2f671f986de7dcdb
fandul <rihards.pacevics@gmail.com>
2015-10-07 09:34:03 +0200

    test class

---
+++ test/JUnit/JUnitTest.java
@@ -0,0 +1,32 @@
+
+package JUnit;
+
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Rihards
+ */
+public class JUnitTest {
+    
+    public JUnitTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+        
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+}

30694677472214e178e391e4668a859285feb484
fandul <rihards.pacevics@gmail.com>
2015-10-07 11:58:53 +0200

    JSON converter for Person

--- src/java/facade/JSONconverter.java
+++ src/java/facade/JSONconverter.java
@@ -4,6 +4,7 @@ import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import entity.Company;
+import entity.Person;
 import java.util.List;
 
 /**
@@ -27,5 +28,20 @@ public class JSONconverter
     public static String getJSONFromCompany(List<Company> companies) //GET LIST
     {
         return gson.toJson(companies);
+    }
+    
+    public static Person getPersonFromJSON(String json) //CREATING, EDITING Person
+    {
+        return gson.fromJson(json, Person.class);
+    }
+    
+    public static String getJSONFromPerson(Person p) //READING, DELETING Person
+    {
+        return gson.toJson(p);
+    }
+    
+    public static String getJSONFromPerson(List<Person> persons) //GET LIST
+    {
+        return gson.toJson(persons);
     }
 }

a2277bb939cc582220046a1226578b98db2b769a
favl <favl.dk@gmail.com>
2015-10-07 11:16:44 +0200

    facade+interface COMPANY

---
+++ src/java/facade/CompanyFacade.java
@@ -0,0 +1,104 @@
+package facade;
+
+import entity.Company;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+
+/**
+ *
+ * @author favl
+ */
+public class CompanyFacade implements CompanyInterface
+{
+    private EntityManagerFactory emf;
+    
+    public CompanyFacade(EntityManagerFactory e)
+    {
+        this.emf = e;
+    }
+    
+    public EntityManager getEntityManager()
+    {
+        return emf.createEntityManager();
+    }
+    
+    @Override
+    public Company createCompany(Company comp)
+    {
+        EntityManager em = getEntityManager();
+        try {
+            em.getTransaction().begin();
+            em.persist(comp);
+            em.getTransaction().commit();
+            return comp;
+        } finally {
+            em.close();
+        }
+    }
+
+    @Override
+    public Company getCompany(int cvr)
+    {
+        EntityManager em = getEntityManager();
+        try {
+            Company c = em.find(Company.class, cvr);
+            //insert exception handling here
+            return c;
+        } finally {
+            em.close();
+        }
+    }
+
+    @Override
+    public Company updateCompany(Company comp)
+    {
+        EntityManager em = getEntityManager();
+        try {
+            Company updated = em.find(Company.class, comp.getId());
+            //insert exception handling here
+            updated.setName(comp.getName());
+            updated.setAddress(comp.getAddress());
+            updated.setCvr(comp.getCvr());
+            updated.setDescription(comp.getDescription());
+            updated.setMarketValue(comp.getMarketValue());
+            updated.setNumOfEmployees(comp.getNumOfEmployees());
+            updated.setEmail(comp.getEmail());
+            updated.setPhones(comp.getPhones());
+            em.getTransaction().begin();
+            em.merge(updated);
+            em.getTransaction().commit();
+            return updated;
+        } finally {
+            em.close();
+        }
+    }
+
+    @Override
+    public Company deleteCompany(Company comp)
+    {
+        EntityManager em = getEntityManager();
+        try {
+            Company c = em.find(Company.class, comp.getId());
+            //insert exception handling here
+            em.getTransaction().begin();
+            em.remove(c);
+            em.getTransaction().commit();
+            return c;
+        } finally {
+            em.close();
+        }
+    }
+
+    @Override
+    public List<Company> getCompanies()
+    {
+        EntityManager em = getEntityManager();
+        try {
+            return em.createQuery("SELECT c FROM Company c").getResultList();
+        } finally {
+            em.close();
+        }
+    }
+    
+}
---
+++ src/java/facade/CompanyInterface.java
@@ -0,0 +1,17 @@
+package facade;
+
+import entity.Company;
+import java.util.List;
+
+/**
+ *
+ * @author favl
+ */
+public interface CompanyInterface
+{
+    public Company createCompany(Company comp);
+    public Company getCompany (int cvr);
+    public Company updateCompany(Company comp);
+    public Company deleteCompany(Company comp);
+    public List<Company> getCompanies();
+}

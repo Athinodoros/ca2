3476e8cd849743c695b65d206dafe00326fbc946
Athinodoros <athinodoros.sgouromallis@hotmail.com>
2015-10-08 10:56:03 +0200

    COMPANY FACADE AND INTERF

---
+++ src/java/facade/CompanyInterface.java
@@ -0,0 +1,18 @@
+package facade;
+
+import entity.Company;
+import exception.CompanyNotFoundException;
+import java.util.List;
+
+/**
+ *
+ * @author favl
+ */
+public interface CompanyInterface 
+{
+    public Company createCompany(Company comp);
+    public Company getCompany (long cvr)throws CompanyNotFoundException;
+    public Company updateCompany(Company comp)throws CompanyNotFoundException;
+    public Company deleteCompany(Company comp)throws CompanyNotFoundException;
+    public List<Company> getCompanies();
+}
